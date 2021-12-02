3a5d83c50ffb2d0ee2414def9965ef2cfdbdc698
Kaushik-Shetti-Kilowott <kaushik.shetti@kilowott.com>
2021-12-02 12:45:03 +0530

    commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static int[] a = { 40, 38, 37, 36, 34, 33, 32, 31, 30, 30, 30, 29, 26, 26, 19 };
+        static void Main(string [] args)
+        {
+            var spObject=new StatsProcessor();
+            spObject.Numbers = a;
+            var result = spObject.GetStatsProcessorResult();
+            Console.WriteLine($"Average:{result.Mean}");
+            Console.WriteLine($"Median:{result.Median}");
+            Console.WriteLine($"Mode:{result.Mode}");
+        }
+    }
+}
---
+++ ConsoleApp1/StatsProcessor.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class StatsProcessor
+    {
+        internal int [] Numbers { private get; set; }
+
+        internal StatsResult GetStatsProcessorResult()
+        {
+            var result=new StatsResult();
+            result.Mean = CalculateAverage();
+            result.Median = CalculateMedian();
+            result.Mode = CalculateMode();
+            return result;
+        }
+
+        //private int CalculateAverage() => Numbers.Sum() / Numbers.Length;
+
+
+        private decimal CalculateAverage()
+        {
+            int i;
+            int sum = 0;
+            for (i = 0; i < Numbers.Length; i++)
+            {
+                sum = sum + Numbers[i];
+            }
+            return (decimal)sum / Numbers.Length;
+        }
+        //private int CalculateMedian()
+        //{
+        //    var s = Numbers.OrderBy(i => i).ToArray();
+        //    var n = s.Length;
+        //    var m = n % 2 == 0
+        //        ? (s[n / 2 - 1] + s[n / 2]) / 2
+        //    :s[n / 2];
+        //    return m;
+        //}
+
+        private int CalculateMedian()
+        {
+            Array.Sort(Numbers);
+            int m = (Numbers.Length) / 2;
+            //Console.WriteLine($"Median :{a[m]}");
+            return Numbers[m];
+        }
+
+        private int CalculateMode()
+        {
+            int sum = 0, maxcount = 0, maxnum = 0;
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                int count = 0;
+                for (int j = 1; j < Numbers.Length - 1; j++)
+                {
+                    if (Numbers[i] == Numbers[j])
+                    {
+                        count++;
+                    }
+                }
+                if (count > maxcount)
+                {
+                    maxcount = count;
+                    maxnum = Numbers[i];
+                }
+            }
+            //Console.WriteLine($"Mode : {maxnum}");
+            return maxnum;
+        }
+
+
+    }
+}
